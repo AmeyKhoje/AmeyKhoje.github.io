a0280072fe7b1f51684bfd7b62d31b995e302564
Amey Khoje <199340@ust-global.com>
2023-07-04 19:27:03 +0530

    added support for scroll

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,6 @@
 let lastScrollTop = 0;
 let debounceTimer: any;
+let pos = 0;
 
 const debounce = (callback: Function, time: number) => {
   window.clearTimeout(debounceTimer);
@@ -78,6 +79,44 @@ function mouseWheelHndler(event: any) {
   }
 }
 
+function touchHandler(event: TouchEvent) {
+  const sections = document.querySelectorAll<HTMLDivElement>('.section');
+  const elements = document.querySelectorAll<HTMLDivElement>('.section');
+  let currentActiveIndex = 0;
+
+  sections.forEach((item, index) => {
+    if (item.classList.contains('active')) {
+      currentActiveIndex = index;
+    }
+  });
+
+  let newPos = event.changedTouches[0].clientY;
+
+  // let e = window.event || event;
+  // let delta = Math.max(-1, Math.min(1, event.wheelDelta || -e.detail));
+  if (newPos > pos) {
+    if (currentActiveIndex === 0) {
+      return;
+    }
+    elements.forEach((item, index) => {
+      if (index === currentActiveIndex) {
+        item.classList.remove('active');
+      }
+      elements[currentActiveIndex - 1].classList.add('active');
+    });
+  } else {
+    if (currentActiveIndex === elements.length - 1) {
+      return;
+    }
+    elements.forEach((item, index) => {
+      if (index === currentActiveIndex) {
+        item.classList.remove('active');
+      }
+      elements[currentActiveIndex + 1].classList.add('active');
+    });
+  }
+}
+
 export function applySinglePageScroll() {
   const sections = document.querySelectorAll<HTMLDivElement>('.section');
   sections[0].classList.add('active');
@@ -85,4 +124,12 @@ export function applySinglePageScroll() {
   window.addEventListener('wheel', (event) =>
     debounce(() => mouseWheelHndler(event), 250)
   );
+
+  window.addEventListener('touchstart', (event: TouchEvent) => {
+    pos = event.changedTouches[0].clientY;
+  });
+
+  window.addEventListener('touchmove', (event: TouchEvent) => {
+    debounce(() => touchHandler(event), 500);
+  });
 }
